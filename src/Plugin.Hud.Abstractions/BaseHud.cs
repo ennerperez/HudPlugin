96f8b70beeb3e625c020812aaf3f1d4c435390f3
Enner Pérez <ennerperez@gmail.com>
2017-04-16 03:20:15 -0400

    Initial source code

---
+++ src/Plugin.Hud.Abstractions/BaseHud.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Plugin.Hud.Abstractions
+{
+    /// <summary>
+    /// Base class for all Hud classes
+    /// </summary>
+    public abstract class BaseHud : IHud, IDisposable
+    {
+        #region IHud
+
+        public abstract void Show();
+
+        public abstract void Show(string message);
+
+        public abstract void Dismiss();
+
+        #endregion IHud
+
+        #region IDisposable
+
+        /// <summary>
+        /// Dispose of class and parent classes
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Dispose up
+        /// </summary>
+        ~BaseHud()
+        {
+            Dispose(false);
+        }
+
+        private bool disposed = false;
+
+        /// <summary>
+        /// Dispose method
+        /// </summary>
+        /// <param name="disposing"></param>
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    //dispose only
+                }
+
+                disposed = true;
+            }
+        }
+
+        #endregion IDisposable
+    }
+}
---
+++ src/Plugin.Hud/CrossHud.cs
@@ -0,0 +1,55 @@
+﻿using Plugin.Hud.Abstractions;
+using System;
+
+namespace Plugin.Hud
+{
+    /// <summary>
+    /// Cross platform Hud implementations
+    /// </summary>
+    public class CrossHud
+    {
+        private static Lazy<IHud> Implementation = new Lazy<IHud>(() => CreateHud(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+        /// <summary>
+        /// Current settings to use
+        /// </summary>
+        public static IHud Current
+        {
+            get
+            {
+                var ret = Implementation.Value;
+                if (ret == null)
+                {
+                    throw NotImplementedInReferenceAssembly();
+                }
+                return ret;
+            }
+        }
+
+        private static IHud CreateHud()
+        {
+#if PORTABLE
+            return null;
+#else
+            return new HudImplementation();
+#endif
+        }
+
+        internal static Exception NotImplementedInReferenceAssembly()
+        {
+            return new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+        }
+
+        /// <summary>
+        /// Dispose of everything
+        /// </summary>
+        public static void Dispose()
+        {
+            if (Implementation != null && Implementation.IsValueCreated)
+            {
+                Implementation.Value.Dispose();
+                Implementation = new Lazy<IHud>(() => CreateHud(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
+            }
+        }
+    }
+}
